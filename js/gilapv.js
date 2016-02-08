af710ab07170785aa3557fa0b60508f9e5cf7d11
Philipp Paulweber <p.paulweber@gmail.com>
2016-02-08 12:08:17 +0100

    Resize
    * automatic resize and canvas re-rendering
    * onload automatic viewport scale detection

--- js/gilapv.js
+++ js/gilapv.js
@@ -32,67 +32,161 @@
 //  WITH THE SOFTWARE.
 //  
 
-var link = $('a[href$=".pdf"]:first').attr('href');
-
-if( link != null ) 
-{
-    var url = window.location.origin + link.replace( "/blob/", "/raw/" );
+function main()
+{    
+    var error = 0;
+    if( !window.jQuery ) 
+    {
+	console.log( 'gilapv: error: could not find jQuery!' );
+	error = error + 1;
+    }
     
-    var $gitlab_pdf = $("<div>", { id : "gitlab-pdf" });
-    var $progress = $("<div>", { class : "progress" });
-    var $progress_bar = $("<div>", { class : "progress-bar", role : "progressbar", style : "width: 1%;" });
-    $progress_bar.attr( "aria-valuenow", "1" );
-    $progress_bar.attr( "aria-valuemin", "0" ); 
-    $progress_bar.attr( "aria-valuemax", "100" );
-    $progress_bar.html( '0% rendered' )
+    if(typeof( $.fn.popover ) == 'undefined')
+    {
+	console.log( 'gilapv: error: could not find Bootstrap!' );
+	error = error + 1;
+    }
+    
+    if( typeof( PDFJS ) == 'undefined' )
+    {
+	console.log( 'gilapv: error: could not find PDF.js!' );
+	error = error + 1;
+    }
+
+    if( error > 0 )
+    {
+	return;
+    }
+    
+    
+    
+    var link = $('a[href$=".pdf"]:first').attr('href');
+    if( link == null ) 
+    {
+	return;
+    }
+
+    var url = window.location.origin + link.replace( '/blob/', '/raw/' );
     
     var fc = $('.file-content');
-    fc.append( $gitlab_pdf );
-    $gitlab_pdf.append( $progress )
+    var dl = $('div[class$="light"]:first');
+    var $progress = $('<div>', { class : 'progress', style : 'width: 60%; float: right;' });
+    dl.append( $progress );
+    var $progress_bar = $('<div>', 
+    { class : 'progress-bar', role : 'progressbar', style : 'width: 0%; color: black;' });    
     $progress.append( $progress_bar );
+    $progress_bar.attr( 'aria-valuenow', '0' );
+    $progress_bar.attr( 'aria-valuemin', '0' ); 
+    $progress_bar.attr( 'aria-valuemax', '100' );
+    $progress_bar.html( '0% rendered' );
 
-    fc.css( 'padding', '10px' );
+    var $gilapv  = $('<div>', { id : 'gilapv' });
+    fc.append( $gilapv );
     
-    var dl = $('h4[class$="light"]:first');
-    dl.css( 'text-align', 'right' );
+    var $gilapv_container  = $('<div>', { id : 'gilapv-container' });
+    $gilapv.append( $gilapv_container );
     
+    function calcView(page)
+    {
+	var scale = 0.1;
+	var viewport = page.getViewport(scale);	    
+	var width = fc.width();
+	while( viewport.width <= width )
+	{
+	    scale = scale + 0.001;
+	    viewport = page.getViewport(scale);
+	}
+	return viewport;
+    }
+    
+    var pages = new Array();
+    var first = true;
+    
+    function renderPage(page,num) 
+    {
+	var viewport = calcView(page);
+    	var canvas = document.createElement('canvas');
+    	var ctx = canvas.getContext('2d');
+    	var renderContext = 
+	{ canvasContext: ctx
+	, viewport: viewport
+	};
+	canvas.height = viewport.height;
+    	canvas.width  = viewport.width;
+	
+	var canvas_id = 'gilapv-page-' + num;
+	canvas.setAttribute( 'id', canvas_id );
+	
+	page.render(renderContext);
+	
+	if( first )
+	{
+	    $gilapv_container.append( canvas );
+	}
+	else
+	{
+	    $( '#' + canvas_id ).replaceWith( canvas );
+	}
+    }
+    
+    function refresh()
+    {
+	for(num = 0; num < pages.length; num++) 
+	{
+	    renderPage( pages[num], num );
+	}
+    }
+    
+    PDFJS.disableWorker = true;
     PDFJS.getDocument(url).then
     ( function getPdf(pdf) 
       {
-    	  for(var num = 1; num <= pdf.numPages; num++) 
+    	  for(num = 1; num <= pdf.numPages; num++) 
     	  {
     	      pdf.getPage(num).then
-    	      ( function renderPage(page) 
-    		{
-    		    var scale = 1.5;
-    		    var viewport = page.getViewport(scale);
-    		    var canvas = document.createElement('canvas');
-    		    var ctx = canvas.getContext('2d');
-    		    var renderContext = {
-    			canvasContext: ctx,
-    			viewport: viewport
-    		    };
-    		    canvas.height = viewport.height;
-    		    canvas.width = viewport.width;
-    		    $('#gitlab-pdf').append(canvas);
-    		    page.render(renderContext);
-    		    var pb = $('.progress-bar');
+    	      ( function processPage(page)
+		{
+		    pages.push( page );
+		    
+		    var pb = $('.progress-bar');
     		    percent = ((num-1) * 100) / (pdf.numPages);
-    		    pb.css( 'width', percent + "%" );
+    		    pb.css( 'width', percent + '%' );
     		    if( percent >= 100 )
     		    {
-                        setTimeout
-                        ( function()
-                          {
-    			      pb.addClass('progress-bar-success');
-                          }
-                        , 500
-                        );
+			setTimeout
+			( function()
+			  {
+    			      pb.addClass( 'progress-bar-success' );
+			  }
+			, 500
+			);
     		    }
-    		    pb.html( percent + "% rendered" );
-    		}
+    		    pb.html( percent + '% rendered' );
+		    
+		    if( pages.length == pdf.numPages )
+		    {
+			refresh();
+			first = false;
+		    }
+		}
     	      );
     	  }
       }
     );
+    
+    $(window).resize
+    ( function()
+      {
+	  clearTimeout( window.resizedFinished );
+	  window.resizedFinished = setTimeout
+	  ( function()
+	    {
+		refresh();
+	    }
+	  , 250
+	  );
+      }
+    );
 }
+
+main();
